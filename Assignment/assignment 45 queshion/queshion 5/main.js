9d207cb7c72db94e2fe5ae292951b36315239bdd
tayyeba-ali <155741204+tayyeba-ali@users.noreply.github.com>
2024-03-12 23:18:34 +0500

    ADD SOON MORE FILE

---
+++ Assignment/assignment 45 queshion/queshion 5/main.js
@@ -0,0 +1,4 @@
+// 5.Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var famous_person = "Bill Gates";
+var message = "".concat(famous_person, "once said, \"Don't compare yourself with anyone in this world ... if you do so, you are insulting yourself.\"");
+console.log(message);
---
+++ Assignment/assignment 45 queshion/queshion 9/main.ts
@@ -0,0 +1,4 @@
+//  9.Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let fav_number:number = 18 ;
+console.log(`My favorite number is: ${fav_number} `);
