6e04e5252bef028228424b69bc4ac89bb3a719c8
Tayyeba Ali <155741204+tayyeba-ali@users.noreply.github.com>
2024-03-28 01:54:44 +0500

    Delete Assignment/assignment 45 queshion directory

--- Assignment/assignment 45 queshion/queshion 3/main.ts
+++
@@ -1,8 +0,0 @@
-//  3.Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
-
-
-let Name:string = "Tayyeba"
-
-console.log(Name.toUpperCase())
-console.log(Name.toLowerCase())
-console.log(Name.charAt(0).toUpperCase()+Name.slice(1).toLowerCase()); 
--- Assignment/assignment 45 queshion/queshion 7/main.ts
+++
@@ -1,6 +0,0 @@
-//  7 .Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
-
-console.log( 4 + 4); //Add
-console.log(16 - 8); // sub
-console.log(4 * 2);// mult
-console.log (32 / 4); // div
